4a191065b67a104a1af4adcdbefacf8139e74d06
raajesh23 <62094385+raajesh23@users.noreply.github.com>
2022-09-27 15:07:20 +0530

    Add files via upload
    1 assignment

---
+++ number comparision/number comparision/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace number_comparision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("\n\n");
+            Console.Write("Check whether two integers are equal or not:\n");
+            Console.Write("-------------------------------------------");
+            Console.Write("\n\n");
+            Console.Write("Input 1st number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input 2nd number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            if (a == b)
+                Console.WriteLine("{0} and {1} are equal.\n", a, b);
+            else
+                Console.WriteLine("{0} and {1} are not equal.\n", a, b);
+            Console.ReadLine();
+        }
+    }
+}
