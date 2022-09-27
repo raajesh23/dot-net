aae1c39b28cf09180cd0d968c1dd1a5f9857b3c6
raajesh23 <62094385+raajesh23@users.noreply.github.com>
2022-09-27 17:43:31 +0530

    Add files via upload
    operation managagement

---
+++ operation manage/operation manage/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace operation_manage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            char operation;
+
+
+
+            Console.Write("Input first number: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Console.Write("Input operation: ");
+
+
+
+            operation = Convert.ToChar(Console.ReadLine());
+            Console.Write("Input second number: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+            if (operation == '+')
+                Console.WriteLine("Excepted output: {0} + {1} = {2}", x, y, x + y);
+
+
+
+
+            else if (operation == '-')
+                Console.WriteLine("Excepted output: {0} - {1} = {2}", x, y, x - y);
+
+
+
+            else if ((operation == 'x') || (operation == '*'))
+                Console.WriteLine("Excepted output: {0} * {1} = {2}", x, y, x * y);
+
+
+
+            else if (operation == '/')
+                Console.WriteLine("Excepted output: {0} / {1} = {2}", x, y, x / y);
+
+
+
+            else
+                Console.WriteLine("Wrong Character");
+            Console.Read();
+        }
+    }
+}
