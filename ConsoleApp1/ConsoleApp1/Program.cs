abe37e8c55babb64f42f18545ef3b7d39ce25c4d
raajesh23 <62094385+raajesh23@users.noreply.github.com>
2022-09-27 17:47:28 +0530

    Add files via upload
    clesius convertion

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius;
+            double fahrenheit = 97;
+            Console.WriteLine("Fahrenheit: " + fahrenheit);
+            celsius = (fahrenheit - 32) * 5 / 9;
+            Console.WriteLine("Celsius: " + celsius);
+            Console.ReadLine();
+        }
+    }
+}
